406d6bb2b506a298e2def2443a6a5095e8f16895
Anders Törnqvist <anders.tornqvist@gmail.com>
2014-05-13 14:05:16 +0200

    Add Logging_formatter

--- lib/puggle.rb
+++ lib/puggle.rb
@@ -1,5 +1,7 @@
 require "puggle/version"
 require "puggle/config"
+require "active_support"
+require "puggle/logging_formatter"
 
 module Puggle
   class << self
---
+++ lib/puggle/logging_formatter.rb
@@ -0,0 +1,24 @@
+module Puggle
+  class LoggingFormatter < ActiveSupport::Logger::Formatter
+    include ActiveSupport::TaggedLogging::Formatter
+
+    def call (severity, timestamp, _progname, msg)
+      unless msg.blank?
+        "[#{process_id}] [#{timestamp}] [#{color_severity(severity)}] #{tags_text}#{msg}\n"
+      end
+    end
+
+    def color_severity (severity)
+      case severity
+      when "ERROR", "FATAL" then severity.red
+      when "WARN" then severity.yellow
+      else
+        severity
+      end
+    end
+
+    def process_id
+      @process_id ||= Process.pid
+    end
+  end
+end
