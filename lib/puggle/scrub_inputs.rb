801673d41dff21ce1d9e1dfd5230deaf4cbb4137
Anders Törnqvist <anders.tornqvist@gmail.com>
2014-05-23 12:17:21 +0200

    Clean up code.

--- lib/puggle/scrub_inputs.rb
+++ lib/puggle/scrub_inputs.rb
@@ -2,18 +2,20 @@ module Puggle
   module ScrubInputs
     def scrub_params(hash)
       hash.inject({}) do |h, (key, value)|
-        h.merge(
-          key => case value
-                 when Hash then scrub_params(value)
-                 when Array then scrup_array(value)
-                 else
-                   scrub_value(value)
-                 end
-        )
+        h.merge(key => scrub(value))
       end
     end
 
     private
+
+    def scrub(value)
+      case value
+      when Hash then scrub_params(value)
+      when Array then scrup_array(value)
+      else
+        scrub_string(value)
+      end
+    end
 
     def scrup_array (array)
       array.map do |value|
@@ -25,11 +27,11 @@ module Puggle
       if value.is_a?(Array)
         scrub_array(value)
       else
-        scrub_value(value)
+        scrub_string(value)
       end
     end
 
-    def scrub_value (value)
+    def scrub_string (value)
       if value.is_a?(String)
         stripped = value.strip
         stripped.blank? ? nil : stripped
