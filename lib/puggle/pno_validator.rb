c3605b6a0070b578afeeaee6758a0cbad3d06c47
Gustaf Hallberg <gustaf@pugglepay.com>
2016-08-17 11:18:24 +0200

    WIP

--- lib/puggle/pno_validator.rb
+++ lib/puggle/pno_validator.rb
@@ -18,6 +18,13 @@ class Puggle::PnoValidator < ActiveModel::Validator
   end
 
   class << self
+
+    def valid_pno? (country_code, pno)
+      valid_format? (country_code, pno) &&
+        valid_date? (country_code, pno) &&
+        valid_checksum(country_code, pno)
+    end
+
     def valid_format? (country_code, pno)
       case country_code
       when 'SE' then pno.match(VALID_SWEDISH_PNO).present?
